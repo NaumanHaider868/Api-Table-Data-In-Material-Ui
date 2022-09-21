e96769d6383a0b9985b3d693eccde0752da0a39c
Nauman Haider <naumanhaider868@gmail.com>
2022-09-21 14:57:52 +0500

    Api Data

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css'
+
+import DataTable from "./Componet/DataTable";
+
+function App() {
+  return (
+    <div className="App">
+      <DataTable/>
+    </div>
+  );
+}
+
+export default App;
